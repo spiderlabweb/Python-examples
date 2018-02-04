95fa16d2dbb3d3c4f085d0f4d5e9de4090b28918
Amit Rawat <georgianamitrawat@gmail.com>
2018-02-04 20:16:00 +0530

    added comments

--- odd-even_number.py
+++ odd-even_number.py
@@ -1,6 +1,6 @@
-num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))    # taking an input
 
-if num%2 == 0:
-    print(num," is an even number.")
+if num%2 == 0:                          # checking whether a number is even
+    print(num," is an even number.")    # if its even then prints even number
 else:
-    print(num," is an odd number.")
+    print(num," is an odd number.")     # else prints odd number
