9481462a81f0f3e0edc01f409e37d2ae51f8a450
Amit Rawat <georgianamitrawat@gmail.com>
2018-02-04 20:38:16 +0530

    prime number

---
+++ prime-number.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))        # taking input from user
+flag = True                                # flag for if a number is prime or not
+
+if num > 1:                                 # the prime no. are greater than 1
+    for i in range(2, num):                 # going through every number btw 2 and num-1
+        if num % i == 0:                    # checking the num is divisible by no.
+            flag = False                    # if does then flag = False, it's not a prime no. 
+            break
+    if flag == False:
+        print("The number is not prime.")   # prints not a prime
+    else:
+        print("The number is prime.")       # prints a prime
+else:
+    print("The number is not prime.")       # prints not a prime
