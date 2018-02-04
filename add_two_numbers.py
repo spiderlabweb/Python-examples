9624f3db124b2f58f4303b5ef741656ac782b356
Amit Rawat <georgianamitrawat@gmail.com>
2018-02-04 19:39:02 +0530

    added the comments

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,6 +1,6 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num1 = int(input("Enter first number: "))   # taking and storing first number in num1 variable
+num2 = int(input("Enter second number: "))  # taking and storing second number in num2 variable
 
-sum = num1 + num2
+sum = num1 + num2                           # adding the two numbers and storing it in sum variable
 
-print("Sum of two numbers is ",sum)
+print("Sum of two numbers is ",sum)         # printing the sum
