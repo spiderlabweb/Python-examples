e8837b208ceb66dca919d11c493ffcbc033dfd00
Amit Rawat <georgianamitrawat@gmail.com>
2018-02-05 09:17:49 +0530

    added comments

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
-num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))        # taking input from user
 
-fac = 1
-for i in range(1,num+1):
-    fac = fac * i
-print("The factorial of number is ",fac)
+fac = 1                                     # initializing the initial value of fac
+for i in range(1,num+1):                    # running for loop 
+    fac = fac * i                           # multiplying previous fac value to i and store it back in fac variable
+print("The factorial of number is ",fac)    # printing the factorial of number
