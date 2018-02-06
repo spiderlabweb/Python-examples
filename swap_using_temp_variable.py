9083238107c8919f706623c9a636108d730ab5d9
Amit Rawat <georgianamitrawat@gmail.com>
2018-02-06 17:43:37 +0530

    swap using temp variable

---
+++ swap_using_temp_variable.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Before swapping:\nFirst Number: ",num1,"\nSecond Number: ",num2)
+
+# swapping these two numbers using temp variable
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("Swapped using temp variable")
+print("After Swapping:\nFirst Number: ",num1,"\nSecond Number: ",num2)
