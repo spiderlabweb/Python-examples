6f9909dac6664e8539f6efe1393c4841d2ad9d69
Amit Rawat <georgianamitrawat@gmail.com>
2018-02-07 17:26:50 +0530

    added comments in swap program

--- swap.py
+++ swap.py
@@ -7,5 +7,5 @@ print("Before swapping\nFirst Number: ",num1,"\nSecond Number: ",num2)
 
 (num1,num2) = (num2,num1)
 
-print("Swapped using temp variable")
+print("Swapped without using temp variable")
 print("After Swapping\nFirst Number: ",num1,"\nSecond Number: ",num2)
