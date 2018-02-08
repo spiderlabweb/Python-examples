d279d77742c745262b0edfa4fe82609790d6de08
Amit Rawat <georgianamitrawat@gmail.com>
2018-02-08 22:50:51 +0530

    celsius to fahrenheit program

---
+++ c_to_f.py
@@ -0,0 +1,5 @@
+celsius = float(input("Enter the temperature in celsius: "))    # taking input in degree celsius
+
+fahrenheit = (celsius * 1.8) + 32                               # calculation fahrenheit
+
+print("The temperature is ",fahrenheit," degree fahrenheit.")   # print fahrenheit
