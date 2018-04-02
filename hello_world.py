d753167a823472d8404fcc3d69057612a1548e57
Amit Rawat <georgianamitrawat@gmail.com>
2018-04-02 21:10:46 +0530

    added different ways to print hello world

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,11 @@
-# prints hello world!
-print("hello world!")   
+# prints hello world on separate line
+print("hello")   
+print("world")
+# prints hello world on separate lines using one print() function
+print("hello","world",sep="\n")
+# prints hello world on the same line
+print("hello world")    # passing as single argument
+print("hello","world")  # passing as two arguments
+# prints hello world on the same line
+print("hello",end=" ")
+print("world")
