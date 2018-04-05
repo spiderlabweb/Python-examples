c67666d14a4b0f83414d01af089219d1578436d3
Amit Rawat <georgianamitrawat@gmail.com>
2018-04-05 20:57:18 +0530

    formatted the program

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -7,6 +7,7 @@ sum = num1 + num2
 # printing the sum
 print("Sum of two numbers is ",sum)  
 
+
 # taking and storing first number in num1 variable
 num1 = float(input("Enter first number: "))   
 # taking and storing second number in num2 variable
