35f434cb83277a39d8d515f2fa14ad2580165820
Amit Rawat <georgianamitrawat@gmail.com>
2018-04-15 19:28:46 +0530

    fibonacci seq program using recursion

---
+++ fibonacci_seq_recursion.py
@@ -0,0 +1,15 @@
+# Recursive function for fibonacci sequence
+def fib(n):
+    if n <=1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+# enter the number of terms of fibonacci sequence to print
+n = int(input("Enter a number: "))     
+
+# calling recursive fib function
+for i in range(n):
+    print(fib(i),end=" ")
+
