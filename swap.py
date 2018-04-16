8512874b0b4462d15a232ab0a354656840f16cc0
Amit Rawat <georgianamitrawat@gmail.com>
2018-04-16 20:08:20 +0530

    added one more way to swap the numbers without using temp variable.

--- swap.py
+++ swap.py
@@ -5,7 +5,12 @@ print("Before swapping\nFirst Number: ",num1,"\nSecond Number: ",num2)
 
 # swapping these two numbers without using temp variable
 
-(num1,num2) = (num2,num1)
+# (num1,num2) = (num2,num1)
+
+# swapping these two numbers without using temp variable. Hence using arithmetic operators
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1 - num2
 
 print("Swapped without using temp variable")
 print("After Swapping\nFirst Number: ",num1,"\nSecond Number: ",num2)
