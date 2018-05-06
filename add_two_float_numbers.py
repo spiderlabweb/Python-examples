5a2811a0fc172a605c4a7b4c9ac3428f5c21aa7c
Amit Rawat <georgianamitrawat@gmail.com>
2018-05-06 23:34:33 +0530

    added two float numbers

---
+++ add_two_float_numbers.py
@@ -0,0 +1,8 @@
+# taking and storing first float number in num1 variable
+num1 = float(input("Enter first number: "))   
+# taking and storing second float number in num2 variable
+num2 = float(input("Enter second number: "))  
+# adding the two numbers and storing it in sum variable
+sum = num1 + num2                           
+# printing the sum
+print("Sum of two numbers is",format(sum,"<20.4f"))  
--- add_two_numbers.py
+++ add_two_numbers.py
@@ -5,14 +5,5 @@ num2 = int(input("Enter second number: "))
 # adding the two numbers and storing it in sum variable
 sum = num1 + num2                           
 # printing the sum
-print("Sum of two numbers is ",sum)  
-
-
-# taking and storing first number in num1 variable
-num1 = float(input("Enter first number: "))   
-# taking and storing second number in num2 variable
-num2 = float(input("Enter second number: "))  
-# adding the two numbers and storing it in sum variable
-sum = num1 + num2                           
-# printing the sum
-print("Sum of two numbers is ",sum)         
+print("Sum of two numbers is",sum)  
+       
