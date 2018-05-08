3e70d93eeee541d839b832fbb168e52f6b445ad0
Amit Rawat <georgianamitrawat@gmail.com>
2018-05-09 03:00:39 +0530

    made odd-even number using bit-wise operator

--- odd-even_number.py
+++ odd-even_number.py
@@ -4,7 +4,7 @@ num = int(input("Enter a number: "))
 # checking whether a number is even
 if num%2 == 0:                          
     # if its even then prints even number
-    print(num," is an even number.")    
+    print(num,"is an even number.")    
 else:
     # else prints odd number
-    print(num," is an odd number.")     
+    print(num,"is an odd number.")     
---
+++ odd-even_number_bit_wise_operator.py
@@ -0,0 +1,10 @@
+# taking an input
+num = int(input("Enter a number: "))    
+
+# checking whether a number is even
+if num&1:                          
+    # if its odd then prints odd number
+    print(num,"is an odd number.")    
+else:
+    # else prints even number
+    print(num,"is an even number.")  
