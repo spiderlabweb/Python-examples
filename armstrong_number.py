cd61b7fa23ba3aa6331c9b6061c2c3802ad0f155
Amit Rawat <georgianamitrawat@gmail.com>
2018-05-09 19:44:10 +0530

    optimized the armstrong number program

--- armstrong_number.py
+++ armstrong_number.py
@@ -3,19 +3,19 @@ num = int(input("Enter a number: "))
 # initializing the sum variable by 0
 sum = 0                      
 # storing the num in temp variable                       
-temp = num                     
+temp = num                  
+   
 # going through every digit of number store in temp variable                     
 while temp > 0:                                     
     n = temp % 10
     temp = temp // 10
     # storing the sum of cube of the digits in sum variable
     sum = sum + pow(n,3)   
-# printing the sum                         
-print("The sum is ",sum)            
+           
 # checking whether the sum is equal to num or not               
 if(sum == num):                 
     # printing the num is an Armstrong number                   
-    print(num," is an Armstrong Number.")          
+    print(num,"is an Armstrong Number.")          
 else:
     # printing the num is not an Armstrong number
-    print(num," is not an Armstrong Number.")  
+    print(num,"is not an Armstrong Number.")  
--- armstrong_number_optimized.py
+++ armstrong_number_optimized.py
@@ -2,14 +2,16 @@
 num = input("Enter a number: ")
 # initializing the sum variable by 0
 sum = 0
+
 # going through every digit of number store in num variable   
 for i in num:
     # storing the sum of cube of the digits in sum variable
     sum += pow(int(i),3)
+
 # checking whether the sum is equal to num or not
 if sum == int(num):
     # printing the num is an Armstrong number 
-    print(num," is an Armstrong Number.")    
+    print(num,"is an Armstrong Number.")    
 else:
     # printing the num is not an Armstrong number
-    print(num," is not an Armstrong Number.")
+    print(num,"is not an Armstrong Number.")
