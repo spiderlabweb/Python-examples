0662edfdfbf78b41e6d81f0afc033ecbe4072825
Amit Rawat <georgianamitrawat@gmail.com>
2018-05-10 20:54:45 +0530

    optimized the odd even program using bitwise operator

--- odd-even_number_bit_wise_operator.py
+++ odd-even_number_bit_wise_operator.py
@@ -1,7 +1,7 @@
 # taking an input
 num = int(input("Enter a number: "))    
 
-# checking whether a number is even
+# checking whether a number is even using bitwise operator
 if num&1:                          
     # if its odd then prints odd number
     print(num,"is an odd number.")    
