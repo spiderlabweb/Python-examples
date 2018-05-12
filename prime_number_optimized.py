1ce8675e068f55d68a29455b5871e0fd54500f9c
Amit Rawat <georgianamitrawat@gmail.com>
2018-05-12 22:43:42 +0530

    optimized prime number program

--- prime_number_optimized.py
+++ prime_number_optimized.py
@@ -1,10 +1,14 @@
+import math
 # taking input from user
 num = int(input("Enter a number: "))         
 
 # the prime no. are greater than 1
 if num > 1:                                 
     # going through every number btw 2 and num-1
-    for i in range(2,num):                 
+    # uncomment the below for loop to check it
+    # for i in range(2,num//2):     
+    # comment the below for loop to check above for loop
+    for i in range(2,math.floor(math.sqrt(num))+1):          
         # checking the num is divisible by i.
         if num % i == 0:                    
             # prints num is not a prime number
