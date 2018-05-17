bbc9ad46a4a594fdb0d6bf50f291b7eda9bf2ed9
Amit Rawat <georgianamitrawat@gmail.com>
2018-05-17 23:48:28 +0530

    made palindrome program for number and string

--- palindrome_of_string_using_split.py
+++ palindrome_of_string_using_split.py
@@ -1,9 +1,9 @@
 # Taking a string from user
 s = input("Enter a string: ")
-# Reversing the string s and storing in rs variable
+# Reversing the string s and storing in rs variable using split
 rs = s[len(s)::-1]
 # Checking whether the given string and reversed string is same
 if s == rs:
-    print("The string is palindrom.")
+    print("The string is palindrome.")
 else:
-    print("The string is not palindrom.")
+    print("The string is not palindrome.")
