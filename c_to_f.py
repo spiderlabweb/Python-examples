8623241839062d2f40629249e8681acbf9f8e4de
Amit Rawat <georgianamitrawat@gmail.com>
2018-05-31 21:53:27 +0530

    extended the program

--- c_to_f.py
+++ c_to_f.py
@@ -3,4 +3,6 @@ celsius = float(input("Enter the temperature in celsius: "))
 # calculation fahrenheit
 fahrenheit = (celsius * 1.8) + 32                               
 # print fahrenheit
-print("The temperature is ",fahrenheit," degree fahrenheit.")   
+print("The temperature is",fahrenheit,"degree fahrenheit.")   
+# print using format() function
+# print("The temperature is {0} degree fahrenheit.".format(fahrenheit))
