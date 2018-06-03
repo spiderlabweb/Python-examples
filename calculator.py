665678569a92d974a9282112e9842669bdc2bae4
Amit Rawat <georgianamitrawat@gmail.com>
2018-06-03 07:46:21 +0530

    completed calculator program

--- calculator.py
+++ calculator.py
@@ -12,11 +12,11 @@ def mul(num1,num2):
 
 # defining div() function to divide the numbers
 def div(num1,num2):
-    return num1 // num2
+    return num1 / num2
 
 print("CALCULATOR")
 print("(1) Addition\n(2) Subtraction\n(3) Multiplication\n(4) Division")
-choice = int(input("Which operation you want to perform? "))
+choice = int(input("Which operation do you want to perform? "))
 # taking and storing first number in num1 variable
 num1 = int(input("Enter first number: "))   
 # taking and storing second number in num2 variable
