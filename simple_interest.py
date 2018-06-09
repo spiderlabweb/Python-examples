e8944023bd1760edd6bed60aa4fa3a79480a7bab
Amit Rawat <georgianamitrawat@gmail.com>
2018-06-09 23:41:05 +0530

    completed simple interest program

--- simple_interest.py
+++ simple_interest.py
@@ -1,1 +1,12 @@
-# 
+# Taking an input for principle
+p = float(input("Enter the principle: ")) 
+# Taking an input for rate per year
+r = float(input("Enter the rate: "))
+# Taking an input for time
+t = float(input("Enter the time(in years): "))
+
+# formula for S.I. is (p*r*t)/100
+si = (p*r*t)/100
+
+# printing the simple interest
+print("The Simple Interest is",si)
