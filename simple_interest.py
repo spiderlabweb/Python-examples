825a6407c0050894b6a277d479a9ae857c97cb70
Amit Rawat <georgianamitrawat@gmail.com>
2018-06-10 09:57:07 +0530

    completed the SI program

--- simple_interest.py
+++ simple_interest.py
@@ -3,7 +3,7 @@ p = float(input("Enter the principle: "))
 # Taking an input for rate per year
 r = float(input("Enter the rate: "))
 # Taking an input for time
-t = float(input("Enter the time(in years): "))
+t = float(input("And Enter the time(in years): "))
 
 # formula for S.I. is (p*r*t)/100
 si = (p*r*t)/100
