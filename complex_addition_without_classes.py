4fcf1670a0e7f435e351b5ac7205425ce0bd7d7c
Amit Rawat <georgianamitrawat@gmail.com>
2019-06-14 14:05:09 +0530

    Complex Number program

---
+++ complex_addition_without_classes.py
@@ -0,0 +1,5 @@
+print("Format for writing complex number: a+bj.\n")
+c1 = complex(input("Enter First Complex Number: "))
+c2 = complex(input("Enter second Complex Number: "))
+
+print("Sum of both the Complex number is", c1 + c2)
